8239ce91e6e36af9fea75d01c98a0a2c9bb84997
arkornilo <120718031+arkornilo@users.noreply.github.com>
2022-12-22 14:09:30 +0300

    Final commit: optimized algorithm, notebook added

--- project_0/score.py
+++ project_0/score.py
@@ -1,43 +1,76 @@
 import numpy as np
+        
 def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
+    """Random number guessing function
 
     Args:
-        number (int, optional): Загаданное число. Defaults to 1.
+        number (int, optional): number to guess. Defaults to 1.
 
     Returns:
-        int: Число попыток
+        int: number of tries
     """
+    
+    number_list = [] # number guess list
+        
+    def guess_number() -> int:
+        """Number guessing optimization algorithm
+        
+        Args: none
+
+        Returns:
+            int: number to guess
+        """
+        
+        global predict_number
+        if number_list == []:
+            predict_number = 50
+        elif number > max(number_list):
+            predict_number = np.random.randint(max(number_list),101)
+        elif number < number_list[::-1][0] and number > np.mean(number_list):
+            predict_number = np.random.randint(np.mean(number_list),number_list[::-1][0])
+        elif number < number_list[::-1][0] and number < np.mean(number_list):
+            predict_number = np.random.randint(1,np.mean(number_list))
+        elif number > number_list[::-1][0] and number < max(number_list):
+            predict_number = np.random.randint(number_list[::-1][0],max(number_list))
+        elif number < number_list[::-1][0] and number < min(number_list):
+            predict_number = np.random.randint(1,min(number_list))
+        else:
+            predict_number = np.random.randint(min(number_list), max(number_list))
+        number_list.append(predict_number)
+        return predict_number
 
     count = 0
-
+    
     while True:
+        """cycle to guess number""" 
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
+        guess_number() # get output from optimization algorithm
         if number == predict_number:
-            break # выход из цикла, если угадали
+            break # cycle exit in case of successful guess
     return(count)
+    
 def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+    """average guesses to guess number for array of 1000 numbers
 
     Args:
-        random_predict ([type]): функция угадывания
+        random_predict ([type]): number guessing function
 
     Returns:
-        int: среднее количество попыток
+        int: average guesses
     """
 
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    count_ls = [] # number of guesses list
+    np.random.seed(1) # fix random seed 
+    random_array = np.random.randint(1, 101, size=(1000)) # generate numbers to be guessed
 
     for number in random_array:
         count_ls.append(random_predict(number))
 
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
+    score = int(np.mean(count_ls)) # average number of guesses 
 
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    print(f'Average number of algorithm guesses: {score}')
     return(score)
 
 # RUN
-score_game(random_predict)
+if __name__ == '__main__':
+    score_game(random_predict)
