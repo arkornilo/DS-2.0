cb485ab5796c3c4a989c3007fe602f7709264d99
arkornilo <120718031+arkornilo@users.noreply.github.com>
2023-02-24 12:25:24 +0300

    py11 & py12

---
+++ test.py
@@ -0,0 +1,20 @@
+import pandas as pd
+items_df = pd.DataFrame({
+    'item_id': [417283, 849734, 132223, 573943, 19475, 3294095, 382043, 302948, 100132, 312394], 
+    'vendor': ['Samsung', 'LG', 'Apple', 'Apple', 'LG', 'Apple', 'Samsung', 'Samsung', 'LG', 'ZTE'],
+    'stock_count': [54, 33, 122, 18, 102, 43, 77, 143, 60, 19]
+})
+purchase_df = pd.DataFrame({
+    'purchase_id': [101, 101, 101, 112, 121, 145, 145, 145, 145, 221],
+    'item_id': [417283, 849734, 132223, 573943, 19475, 3294095, 382043, 302948, 103845, 100132], 
+    'price': [13900, 5330, 38200, 49990, 9890, 33000, 67500, 34500, 89900, 11400]
+})
+merged = items_df.merge(
+    purchase_df,
+    on='item_id',
+    how='inner'
+)
+merged['revenue']=merged['stock_count']*merged['price']
+income=sum(merged['revenue'])
+print(income)
+                  
